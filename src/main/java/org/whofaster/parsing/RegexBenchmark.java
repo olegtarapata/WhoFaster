553c49115f0e08ffe388dcce7018fbb1e8a2ba7c
oleg_tarapata <o.tarapata@epom.com>
2018-02-12 19:47:00 +0200

    #1 Initialize maven project.

---
+++ src/main/java/org/whofaster/parsing/RegexBenchmark.java
@@ -0,0 +1,14 @@
+package org.whofaster.parsing;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+/**
+ * @author Oleg Tarapata (o.tarapata@epom.com)
+ */
+public class RegexBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        // Currently without implementation
+    }
+}
