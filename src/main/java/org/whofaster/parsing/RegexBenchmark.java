79500b1734500fd9868fc614bf43afae7fdeef43
olegtarapata <oleh.tarapata@gmail.com>
2018-02-14 00:45:58 +0200

    #2 Add first benchmark for email validation by regex.

--- src/main/java/org/whofaster/parsing/RegexBenchmark.java
+++ src/main/java/org/whofaster/parsing/RegexBenchmark.java
@@ -1,6 +1,17 @@
 package org.whofaster.parsing;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 /**
  * @author Oleg Tarapata (oleh.tarapata@gmail.com)
@@ -8,7 +19,17 @@ import org.openjdk.jmh.annotations.Benchmark;
 public class RegexBenchmark {
 
     @Benchmark
-    public void testMethod() {
-        // Currently without implementation
+    @Fork(value = 1)
+    @BenchmarkMode(Mode.AverageTime)
+    @Warmup(iterations = 5, time = 1)
+    @Measurement(iterations = 10, time = 1)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public boolean parseEmail(PatternHolder patternHolder) {
+        return patternHolder.pattern.matcher("test@email.com").matches();
+    }
+
+    @State(Scope.Benchmark)
+    public static class PatternHolder {
+        Pattern pattern = Pattern.compile(".*@.*\\..*");
     }
 }
