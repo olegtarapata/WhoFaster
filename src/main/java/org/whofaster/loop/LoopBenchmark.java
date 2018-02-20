22cd8f42d045e599c644b9a49da8a22ff5f002f1
olegtarapata <oleh.tarapata@gmail.com>
2018-02-20 23:32:35 +0200

    #3 Add different sizes for loop benchmarks. Add foreach and stream benchmarks for array.

--- src/main/java/org/whofaster/loop/LoopBenchmark.java
+++ src/main/java/org/whofaster/loop/LoopBenchmark.java
@@ -6,25 +6,53 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author Oleg Tarapata (oleh.tarapata@gmail.com)
  */
-@Fork(value = 1)
 @BenchmarkMode(Mode.AverageTime)
-@Warmup(iterations = 5, time = 1)
-@Measurement(iterations = 10, time = 1)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Benchmark)
 public class LoopBenchmark {
 
+    private int[] array;
+
+    @Param({"10", "100", "1000", "10000", "100000"})
+    private int n;
+
+    @Setup
+    public void setup() {
+        array = new int[n];
+    }
+
     @Benchmark
-    public void usualLoop(Blackhole blackhole) {
+    public void usualLoop(final Blackhole blackhole) {
         for (int i = 0; i < 1000; i++) {
             blackhole.consume(i);
         }
+    }
+
+    @Benchmark
+    public void arrayForeachLoop(final Blackhole blackhole) {
+        for (int anArray : array) {
+            blackhole.consume(anArray);
+        }
+    }
+
+    @Benchmark
+    public void arrayStreamLoop(final Blackhole blackhole) {
+        Arrays.stream(array).forEach(blackhole::consume);
     }
 }
