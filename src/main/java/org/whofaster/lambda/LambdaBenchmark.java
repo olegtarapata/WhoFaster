592a32e1496a6745a075a278c3a76c2c2a056d9b
Oleg Tarapata <oleh.tarapata@gmail.com>
2018-02-26 22:59:08 +0200

    #0 Add lambda benchmark

---
+++ src/main/java/org/whofaster/lambda/LambdaBenchmark.java
@@ -0,0 +1,21 @@
+package org.whofaster.lambda;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 10, time = 1)
+@Fork(1)
+@State(Scope.Benchmark)
+public class LambdaBenchmark {
+}
